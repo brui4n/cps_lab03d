4073ea2cb8de46aef6cc3bd3927be0a80b1f10fd
milennne <milene.fuentes@tecsup.edu.pe>
2025-09-01 15:02:58 -0500

    Commit en repositories de Milene

--- src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
+++ src/main/java/pe/edu/tecsup/lab03/repositories/StudentRepository.java
@@ -1,4 +1,7 @@
 package pe.edu.tecsup.lab03.repositories;
 
 public class StudentRepository {
+    public static void main(String[] args){
+        System.out.println("Cambios en studentrepository de Milene");
+    }
 }
