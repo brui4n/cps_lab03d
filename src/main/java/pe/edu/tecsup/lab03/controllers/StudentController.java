d13f0dbc329c1e6d1bfa002b1e5c68e24b2754e9
Bryan <bryan@MacBook-Air-BC.local>
2025-09-01 15:12:43 -0500

    StudenController (modificacion) - Bryan Coronel

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,5 +1,7 @@
 package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
-    // Hola mundo
+    public static void main(String[] args) {
+        System.out.println("Hola mundo - Bryan Coronel");
+    }
 }
